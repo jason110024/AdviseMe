d7df72d84977c0fab120cbd17d943078d862634a
Jason <jason.anthraper@utexas.edu>
2014-04-21 11:09:12 -0500

    Finished Servlet

--- AdviseMe UT/src/webapp/addServlets/addPostServlet.java
+++ AdviseMe UT/src/webapp/addServlets/addPostServlet.java
@@ -1,6 +1,7 @@
 package webapp.addServlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +15,34 @@ import com.googlecode.objectify.ObjectifyService;
 public class addPostServlet extends HttpServlet{
 	static{ObjectifyService.register(Post.class);}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-		
-		
+		String userId = req.getParameter("id");
+		String username = req.getParameter("username");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+		String tags = req.getParameter("tags");
+		ArrayList<String> tag = new ArrayList<String>(0);
+		Post post;
 		try{
-		
-			
-			resp.sendRedirect("/home.jsp");
+			if(userId==null||userId.isEmpty()){
+				throw new Exception("User Identification not sent!");
+			}
+			if(username==null||username.isEmpty()){
+				throw new Exception("User Info not sent!");
+			}
+			if(title==null||title.isEmpty()){
+				throw new Exception("Must provide a Title!");
+			}
+			if(content==null||content.isEmpty()){
+				throw new Exception("Must provide a Content!");
+			}
+			if(tags==null||tags.isEmpty()){
+				post=new Post(userId,username,title,content);
+			}else{
+				tag.add(tags);
+				post = new Post(userId,username,title,content,tag);
+			}
+			ObjectifyService.ofy().save().entity(post).now();
+			resp.sendRedirect("/forum.jsp");
 		
 		} catch (Exception e){
 			String logMsg = "Exception in processing request: " + e.getMessage();
