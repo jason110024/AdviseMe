2384802454de818ac2ca7c9940f994b87eee1001
Jason <jason.anthraper@utexas.edu>
2014-04-28 22:44:44 -0500

    fixed stupid stufffffff

--- AdviseMe UT/src/webapp/datastoreObjects/Subscriber.java
+++ AdviseMe UT/src/webapp/datastoreObjects/Subscriber.java
@@ -1,13 +1,22 @@
 package webapp.datastoreObjects;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
 public class Subscriber {
-	private String email;
+	@Id private String email;
+	
+	public Subscriber(){}
+	
 	public Subscriber(String email){
 		this.email=email;
 	}
+	
 	public String getEmail(){
 		return this.email;
 	}
+	
 	public void setEmail(String email){
 		this.email=email;
 	}
